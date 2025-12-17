42202e05222901039c422a7120f97eb0476314cc
Guilherme Eras <135174494+Sifechan@users.noreply.github.com>
2025-12-17 09:20:27 -0300

    17122025

--- 17122025 Battle of Arrays.py
+++ 17122025 Battle of Arrays.py
@@ -1,30 +1,36 @@
-for t in range(int(input())):
+import heapq
 
+for t in range(int(input())):
     agrs = list(map(int, input().split()))
-    arrA = list(map(int, input().split()))
-    arrB = list(map(int, input().split()))
+    
+    arrA = [-x for x in map(int, input().split())]
+    arrB = [-x for x in map(int, input().split())]
+    
+    heapq.heapify(arrA)
+    heapq.heapify(arrB)
 
     turn = 1
 
     while len(arrA) > 0 and len(arrB) > 0:
-        Alice_max = max(arrA)
-        Bob_max = max(arrB)
+
+        Alice_max = -heapq.heappop(arrA)
+        Bob_max = -heapq.heappop(arrB)
 
         if turn == 1:
             if Alice_max >= Bob_max:
-                arrB.remove(Bob_max)
+                heapq.heappush(arrA, -Alice_max)
             else:
-                arrB.remove(Bob_max)
-                arrB.append(Bob_max - Alice_max)
+                heapq.heappush(arrB, -(Bob_max - Alice_max))
+                heapq.heappush(arrA, -Alice_max)
         
             turn = 0
 
         else:
             if Bob_max >= Alice_max:
-                arrA.remove(Alice_max)
+                heapq.heappush(arrB, -Bob_max)
             else:
-                arrA.remove(Alice_max)
-                arrA.append(Alice_max - Bob_max)
+                heapq.heappush(arrA, -(Alice_max - Bob_max))
+                heapq.heappush(arrB, -Bob_max)
         
             turn = 1
         
